4a8be3de7336907818ef32379b1ac287340dd48f
Mahdi Ahmadi Kamal <909stars@gmail.com>
2025-05-14 21:12:46 +0330

    :

--- Emoji_Text_Classification.py
+++ Emoji_Text_Classification.py
@@ -8,3 +8,10 @@ import pandas as pd
 class EmojiTextClassifier:
     def __init__(self, arg):
         self.dimension = arg.dimension
+
+    def load_dataset(self, file_path):
+        df = pd.read_csv(file_path)
+        X = np.array(df["sentence"])
+        Y = np.array(df["label"], dtype=int)
+        return X, Y
+
